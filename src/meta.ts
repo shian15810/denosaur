87d625072d4a1754c5a0c989d7a7b949f779cc1a
Chuah Chee Shian <shian15810@gmail.com>
2020-04-24 03:34:54 +0800

    save progress

--- src/meta.ts
+++ src/meta.ts
@@ -5,9 +5,15 @@ import * as what from "pika:is-what";
 
 import * as deno from "./deno.ts";
 
-type Denosaur = { dependencies?: { [dependency: string]: string } };
+type RawDenosaur = { dependencies?: { [dependency: string]: string } };
+type DenosaurDependencies = {
+  [registry: string]: { [module: string]: string };
+};
+type Denosaur = RawDenosaur & { _dependencies: DenosaurDependencies };
 
-type Importmap = { imports?: { [map: string]: string } };
+type RawImportmap = { imports?: { [map: string]: string } };
+type ImportmapImports = { [registry: string]: { [module: string]: string } };
+type Importmap = RawImportmap & { _imports: ImportmapImports };
 
 const getRoot = async (cwd: string): Promise<string | undefined> => {
   const dirname = path.dirname(cwd);
@@ -18,23 +24,40 @@ const getRoot = async (cwd: string): Promise<string | undefined> => {
   return getRoot(dirname);
 };
 
-const isDenosaur = (denosaur: unknown): denosaur is Denosaur =>
+const isRawDenosaur = (denosaur: unknown): denosaur is RawDenosaur =>
   what.isPlainObject(denosaur) &&
   (denosaur.dependencies === undefined ||
     (what.isPlainObject(denosaur.dependencies) &&
       Object.values(denosaur.dependencies).every(
-        (range) => typeof range === "string",
+        (version) => typeof version === "string",
       )));
 const getDenosaur = async (root: string): Promise<Denosaur | undefined> => {
   const filename = path.resolve(root, "denosaur.json");
   const exists = await fs.exists(filename);
   if (!exists) return undefined;
   const denosaur = await fs.readJson(filename);
-  if (!isDenosaur(denosaur)) return undefined;
-  return denosaur;
+  if (!isRawDenosaur(denosaur)) return undefined;
+  const rawDenosaur = Object.entries(denosaur).reduce(
+    (raw: RawDenosaur, [key, value]) =>
+      key.startsWith("_") ? raw : { ...raw, [key]: value },
+    {},
+  );
+  const _dependencies = Object.entries(rawDenosaur.dependencies ?? {}).reduce(
+    (dependencies: DenosaurDependencies, [dependency, version]) => {
+      const deps = dependency.split(":");
+      if (deps.length !== 2 || deps.includes("")) return dependencies;
+      const [registry, module] = deps;
+      return {
+        ...dependencies,
+        [registry]: { ...(dependencies[registry] ?? {}), [module]: version },
+      };
+    },
+    {},
+  );
+  return { ...rawDenosaur, _dependencies };
 };
 
-const isImportmap = (importmap: unknown): importmap is Importmap =>
+const isRawImportmap = (importmap: unknown): importmap is RawImportmap =>
   what.isPlainObject(importmap) &&
   (importmap.imports === undefined ||
     (what.isPlainObject(importmap.imports) &&
@@ -46,8 +69,25 @@ const getImportmap = async (root: string): Promise<Importmap | undefined> => {
   const exists = await fs.exists(filename);
   if (!exists) return undefined;
   const importmap = await fs.readJson(filename);
-  if (!isImportmap(importmap)) return undefined;
-  return importmap;
+  if (!isRawImportmap(importmap)) return undefined;
+  const rawImportmap = Object.entries(importmap).reduce(
+    (raw: RawImportmap, [key, value]) =>
+      key.startsWith("_") ? raw : { ...raw, [key]: value },
+    {},
+  );
+  const _imports = Object.entries(rawImportmap.imports ?? {}).reduce(
+    (imports: ImportmapImports, [map, url]) => {
+      const maps = map.split(":");
+      if (maps.length !== 2 || maps.includes("")) return imports;
+      const [registry, module] = maps;
+      return {
+        ...imports,
+        [registry]: { ...(imports[registry] ?? {}), [module]: url },
+      };
+    },
+    {},
+  );
+  return { ...rawImportmap, _imports };
 };
 
 class Meta {
--- src/registries/unpkg.ts
+++ src/registries/unpkg.ts
@@ -1,18 +1,14 @@
-type Dependencies = { [module: string]: string };
-
 type Registry = {};
 
 class Unpkg {
-  #dependencies: Dependencies = {};
   #registry: Registry = {};
   #inited = false;
 
-  init = async (dependencies: Dependencies): Promise<void> => {
+  init = async (modules: string[]): Promise<void> => {
     await Promise.resolve();
 
     if (this.#inited) return;
 
-    this.#dependencies = dependencies;
     this.#inited = true;
   };
 }
