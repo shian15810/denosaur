328953c4c58eee0faa7f3451df416bc31d85cee3
Chuah Chee Shian <shian15810@gmail.com>
2020-04-26 23:54:16 +0800

    revert commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
-import commands, { isCommand } from "./commands/mod.ts";
-import { flags, permissions } from "./deno.ts";
-
-const main = async (): Promise<void> => {
-  await permissions();
-  const [command, ...inputs] = flags()._;
-  if (isCommand(command)) return commands[command](inputs);
-};
+const main = (): Promise<void> => Promise.resolve();
 
 export default main;
