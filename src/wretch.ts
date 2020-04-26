6d9403e34463c2d30176d902ad2635d608aebf47
Chuah Chee Shian <shian15810@gmail.com>
2020-04-26 23:56:03 +0800

    revert commit

--- src/wretch.ts
+++
@@ -1,7 +0,0 @@
-import wretch from "pika:wretch";
-
-const npmRegistry = wretch("https://registry.npmjs.org").accept(
-  "application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*",
-);
-
-export { npmRegistry };
